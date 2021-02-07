f9b18983f3201fa8c43ff3fbc289d908f020b50a
mrtlgz <47647189+mrtlgz@users.noreply.github.com>
2021-02-07 21:20:29 +0300

    Create 1.py

---
+++ python/1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import requests
+from termcolor import colored
+from datetime import datetime
+
+
+now = str(datetime.now())
+
+url = 'http://localhost'
+uri = '/login.php'
+url = url + uri
+
+param = '?doc=/bin/ls'
+full_req: str= url + param
+req = requests.get(full_req)
+print(colored(full_req + "\nIsteğiniz gönderiliyor...", 'green'))
+
+file = open("/var/log/usertest/user.log", "a+")
+if req:
+    print(colored('The rule is not correct. Please check...', 'red', attrs=['reverse', 'blink']))
+    file.write(full_req + ' ' + now + " Try again! 1\n")
+else:
+    print(colored('The rule is correct!', 'blue', attrs=['reverse', 'blink']))
+    file.write(full_req + ' ' + now +" Succeeded 1 \n")
+file.close()
